689bfd9a88d0b0ad70ed3c5ba1b4af0b4c318281
Moniruzzaman <monir150227@gmail.com>
2020-11-27 20:50:17 +0600

    Initial commit

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+const router = express.Router();
+import protect from "../middleware/authMiddleware.js";
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userControllers.js";
+
+router.route("/").post(registerUser);
+router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile);
+
+export default router;
